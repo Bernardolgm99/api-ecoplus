835ea0b0a622edee093599c03bed20fda92b84b6
Bernardolgm99 <bernardolgm99@gmail.com>
2023-05-26 12:55:43 +0100

    rating count query

--- controllers/rating.controller.js
+++ controllers/rating.controller.js
@@ -4,9 +4,16 @@ const messages = require("../utilities/messages");
 const validation = require("../utilities/validation.js");
 
 exports.getAll = async (req, res) => {
-  let rating = await Rating.findAll();
-  
-  res.status(200).json(rating);
+  try {
+    await db.sequelize.query(
+      `SELECT COUNT(CASE WHEN rating = true THEN 1 END) - COUNT(CASE WHEN rating = false THEN 1 END) AS result FROM ratings WHERE commentId = ${req.params.commentId}`
+    ).then(result => {
+      res.json(result[0][0])
+    })
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(messages.errorInternalServerError());
+  };
 };
 
 exports.like = async (req, res) => {
@@ -25,19 +32,19 @@ exports.like = async (req, res) => {
       await rating.update({ like: req.body.rating });
       res.status(200).send({ msg: `Like Updated`, rating: rating.like });
     }
-} catch (err) {
+  } catch (err) {
     res.status(500).json(messages.errorInternalServerError());
-}
+  }
 };
 
 exports.delete = async (req, res) => {
-    try {
-        let rating = await Rating.findOne({
-            where: { commentId: req.params.commentId, userId: req.loggedUser.id },
-        });
-        await rating.delete( {where: { commentId: req.params.commentId, userId: req.loggedUser.id}} )    
-        res.status(200).send({ msg: `Like Deleted`, rating: null });
-} catch (err) {
+  try {
+    let rating = await Rating.findOne({
+      where: { commentId: req.params.commentId, userId: req.loggedUser.id },
+    });
+    await rating.delete({ where: { commentId: req.params.commentId, userId: req.loggedUser.id } })
+    res.status(200).send({ msg: `Like Deleted`, rating: null });
+  } catch (err) {
     res.status(500).json(messages.errorInternalServerError());
   }
 };
