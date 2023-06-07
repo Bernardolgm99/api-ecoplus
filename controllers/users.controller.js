07b78b01461b32d0b50dbcbc3ee6fa9abfbe8538
NarizConstipado <gaspar.antunesvieira@gmail.com>
2023-06-07 10:00:15 +0100

    users findAll and one return only the id of badges, events, occurrences, activities. findall only returns a short number of the user caracteristics

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -117,7 +117,7 @@ exports.findAll = async (req, res) => {
 
 exports.findOne = async (req, res) => {
   try {
-    if (req.loggedUser == undefined) findUser = await User.findOne({ where: { id: req.params.userId }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }] }, {})
+    if (req.loggedUser == undefined) findUser = await User.findOne({ where: { id: req.params.userId }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }] }, {})
     else findUser = await User.findOne({ where: { id: req.loggedUser.id }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }] }, {})
     if (findUser != null) {
       res.status(200).json({
