ba4ecc4a348073efa58d0c93144a5b74b6135832
NarizConstipado <gaspar.antunesvieira@gmail.com>
2023-06-07 10:22:45 +0100

    user find one returns all badges, occurrences, activities and events

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -117,8 +117,8 @@ exports.findAll = async (req, res) => {
 
 exports.findOne = async (req, res) => {
   try {
-    if (req.loggedUser == undefined) findUser = await User.findOne({ where: { id: req.params.userId }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }] }, {})
-    else findUser = await User.findOne({ where: { id: req.loggedUser.id }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }] }, {})
+    if (req.loggedUser == undefined) findUser = await User.findOne({ where: { id: req.params.userId }, include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }, { model: db.activity, attributes: ['id'], through: { attributes: [] } }, { model: db.occurrence, attributes: ['id']}]})
+    else findUser = await User.findOne({ where: { id: req.loggedUser.id },include: [{ model: Badge, attributes: ['id'], through: { attributes: [] } }, { model: db.event, attributes: ['id'], through: { attributes: [] } }, { model: db.activity, attributes: ['id'], through: { attributes: [] } }, { model: db.occurrence, attributes: ['id']}] }, {})
     if (findUser != null) {
       res.status(200).json({
         sucess: true,
