92f90b86d288d79e4dc5daa095560b607ddf9101
Bernardolgm99 <bernardolgm99@gmail.com>
2023-06-15 12:51:23 +0100

    correction eventsOccurrences controller

--- controllers/eventsOccurrences.controller.js
+++ controllers/eventsOccurrences.controller.js
@@ -8,16 +8,16 @@ const messages = require('../utilities/messages');
 exports.findAll = async (req, res) => {
     try {
         let page = 0, limit = 5, createdAt = new Date();
-        console.log(createdAt);
         if (req.query.page)
-            page = +req.query.page;
-
+        page = +req.query.page;
+        
         if (req.query.limit)
-            limit = +req.query.limit;
-
+        limit = +req.query.limit;
+        
         if (req.query.createdAt)
-            createdAt = req.query.createdAt;
-
+        createdAt = req.query.createdAt;
+        
+        console.log(createdAt, limit, page);
         if (typeof (page) != 'number') { res.status(400).json(messages.errorBadRequest(0, "page", "number")); return; };
 
         if (typeof (limit) != 'number') { res.status(400).json(messages.errorBadRequest(0, "limit", "number")); return; };
@@ -57,31 +57,22 @@ exports.findAll = async (req, res) => {
             }],
         })
 
-        events.forEach(event => {
-            event.image = event.image.toString('base64');
-            event.files = event.files.toString('base64');
-        })
-
-        occurrences.forEach(occurrence => {
-            occurrence.image = occurrence.image.toString('base64');
-        })
-
         let eventsOccurrences = events.concat(occurrences);
 
         eventsOccurrences = eventsOccurrences.sort((a, b) => b.createdAt - a.createdAt)
         eventsOccurrences = eventsOccurrences.slice(0, limit)
 
         eventsOccurrences.forEach(eventOccurrence => {
-            eventOccurrence.image = eventOccurrence.image.toString('base64')
-            if(eventOccurrence.files) {
+            if (eventOccurrence.image)
+                eventOccurrence.image = eventOccurrence.image.toString('base64')
+            if(eventOccurrence.files)
                 eventOccurrence.files = eventOccurrence.files.toString('base64')
-            }
         })
 
         res.status(200).json(eventsOccurrences);
 
     } catch (err) {
-        console.log(err);
+        // console.log(err);
         res.status(500).json(messages.errorInternalServerError());
     };
 };
