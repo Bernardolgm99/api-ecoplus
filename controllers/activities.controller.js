33e639ac69e7e54381072a116758a6b598b4ca9d
Bernardolgm99 <bernardolgm99@gmail.com>
2023-06-15 20:54:08 +0100

    activities changes

--- controllers/activities.controller.js
+++ controllers/activities.controller.js
@@ -35,7 +35,7 @@ exports.findAll = async (req, res) => {
 
     if (typeof (limit) != 'number') { res.status(400).json(messages.errorBadRequest(0, "Limit", "number")); return; };
 
-    let activities = await Activity.findAll({ order: [['createdAt', 'DESC']], offset: page, limit: limit, include: [{ model: db.comment, offset: 0, limit: 2, order: [['createdAt', 'DESC']], include: { model: User, attributes: ['username'] } }] });
+    let activities = await Activity.findAll({ offset: page, limit: limit, include: [{ model: db.comment, offset: 0, limit: 2, order: [['createdAt', 'DESC']], include: { model: User, attributes: ['username'] } }] });
 
 
     activities.forEach(activity => {
