0317a3ca87b4821096ae300dad1d6472939930c6
Eduardo Alves <edualves884@gmail.com>
2023-06-15 13:43:36 +0100

    y

--- config/db.config.js
+++ config/db.config.js
@@ -7,7 +7,7 @@ const config = {
     dialect: "mysql",
     // pool is optional, it will be used for Sequelize connection pool configuration
     pool: {
-        max: 5, //maximum number of connections in pool
+        max: 10, //maximum number of connections in pool
         min: 0, //minimum number of connections in pool
         acquire: 30000,//maximum time (ms), that pool will try to get connection before throwing error
         idle: 10000 //maximum time (ms) that a connection can be idle before being released
